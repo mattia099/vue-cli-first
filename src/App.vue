527ae956b14297ff12a973c220c33168e1b80473
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-03-08 10:30:05 +0100

    componente title

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitleTitle /> 
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TitleTitle from './components/TitleTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TitleTitle
   }
 }
 </script>
---
+++ src/components/TitleTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{title}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TitleTitle',
+    data(){
+        return{
+            title:'La mia prima app con Vue Cli'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
